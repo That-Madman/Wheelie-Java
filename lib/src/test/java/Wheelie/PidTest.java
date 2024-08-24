e8987cfdbf1d95c0b1708b3891cb028616e8ce8a
That-Madman <That-Madman@Github>
2024-08-24 16:50:41 -0400

    Finally the dummy classes are gone

---
+++ lib/src/test/java/Wheelie/PidTest.java
@@ -0,0 +1,11 @@
+package Wheelie;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertations.*;
+
+class PidTest {
+	@Test void PidCalc () { //TODO: Get this to actually test instead of being a placeholder
+		PID PidClass = new PidClass (0.0, 0.0, 0.0);
+		assertTrue (true, "Return Statement");
+	}
+}

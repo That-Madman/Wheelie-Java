628d18cdd593b21035680e91f7c27b8d84006ebe
That-Madman <That-Madman@Github>
2024-08-25 10:38:26 -0400

    Tests for Pose2D

--- lib/src/test/java/Wheelie/PidTest.java
+++ lib/src/test/java/Wheelie/PidTest.java
@@ -1,11 +1,11 @@
 package Wheelie;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertations.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PidTest {
 	@Test void PidCalc () { //TODO: Get this to actually test instead of being a placeholder
-		PID PidClass = new PidClass (0.0, 0.0, 0.0);
+		PID PidClass = new PID (0.0, 0.0, 0.0);
 		assertTrue (true, "Return Statement");
 	}
 }

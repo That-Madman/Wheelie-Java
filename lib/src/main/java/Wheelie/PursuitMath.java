ab07b8a96d80f02e4c5dee0358f4e34a1d631dbb
That-Madman <That-Madman@Github>
2024-08-25 19:13:01 -0400

    Pure Pursuit grows closer

--- lib/src/main/java/Wheelie/PursuitMath.java
+++ lib/src/main/java/Wheelie/PursuitMath.java
@@ -50,9 +50,44 @@ public final class PursuitMath {
 	 *
 	 * @author Alex Bryan
 	 */
-	public static double PureQuadForm (double a, double b, double c) {
+	public static double pureQuadForm (double a, double b, double c) {
 		double disc = (b * b) - (4 * a * c);
 		//Thanks to Team 23423 for the information that the sum is the closer point
 		return (0 > disc) ? Double.NaN : (-b + Math.sqrt(disc)) / (2 * a);
+	}
+
+	/** Calculates a single part of a waypoint, such as X, Y or the Heading */
+	public static double waypointCompCalc (double c1, double c2, double q) {
+		return c1 + q * (c2 - c1);
+	}
+
+	/**
+	 * Calculates a full waupoint for the path.
+	 * @param obj The location of the robot, AKA the center of the circle
+	 * @param look The lookahead distance, AKA the radius of the circle
+	 * @param p1 The first of two preceding points
+	 * @param p2 The second of two preceding points
+	 *
+	 * @author Alex Bryan
+	 */
+	public static Pose2D waypointCalc (Pose2D obj, double look, Pose2D p1, Pose2D p2) {
+		double t = pureQuadForm (
+				(p2.getX() - p1.getX()) * (p2.getX() - p2.getX()) + (p2.getY() - p1.getY()) * (p2.getY() - p2.getY()),
+				2 * ((p1.getX() - obj.getX()) * (p2.getX() - p2.getX()) + (p1.getY() - obj.getY()) * (p2.getY() - p1.getY())),
+				p1.getX() * p1.getX() - 2 * obj.getX() * p1.getX() + obj.getX() * obj.getX() + p1.getY() * p1.getY() - 2 * obj.getY() * p1.getY() + obj.getY() * obj.getY() - look * look
+				);
+		
+		if (t == t) //check if non-NaN
+			return new Pose2D (
+				waypointCompCalc (p1.getX(), p2.getX(), t),
+				waypointCompCalc (p1.getY(), p2.getY(), t),
+				waypointCompCalc (p1.getH(), p2.getH(), t)
+				);
+		else
+			return new Pose2D (
+					Double.NaN,
+					Double.NaN,
+					Double.NaN
+					);
 	}
 }
